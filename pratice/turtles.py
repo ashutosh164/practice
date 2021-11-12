4d3c22339d38a6a4f36f4409847ccee186bcc35a
Ashutosh Pradhan <ashutoshpradhan164@gmail.com>
2021-11-12 16:31:45 +0530

    create turtles

---
+++ pratice/turtles.py
@@ -0,0 +1,36 @@
+from turtle import *
+import turtle
+
+# speed(12)
+# color('cyan')
+# bgcolor('black')
+# b = 200
+# while b > 0:
+#     left(b)
+#     forward(b * 3)
+#     b = b-1
+
+# color('red')
+# bgcolor('black')
+# begin_fill()
+# pensize(3)
+# left(50)
+# forward(133)
+# circle(50,200)
+# right(140)
+# circle(50,200)
+# forward(133)
+# end_fill()
+
+
+screen = turtle.Screen()
+turtle.bgcolor('black')
+color = ['forestgreen','crimson']
+screen.setworldcoordinates(-1000,-1000,1000,1000)
+turtle.speed(0)
+turtle.hideturtle()
+# for i in range(20,800,5):
+    # print(i)
+
+
+
